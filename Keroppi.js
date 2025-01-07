b35273c1305c7058d7ad7d4e7131e3ba3fe973d5
Gabrielle Barthelmy <143374150+303gabby@users.noreply.github.com>
2025-01-07 01:05:04 -0500

    Create Keroppi.js

---
+++ Keroppi.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(230, 230, 250);
+  
+  strokeWeight(7);
+   stroke(0,0,0);
+  fill(217, 227, 71);
+  ellipse(350,320,350,270);
+  fill(255);
+  circle(285,200,135);
+  circle(421,200,135);
+  fill(0,0,0);
+ 
+  circle(310,200,30);
+  circle(400,200,30);
+  stroke(243, 179, 194);
+  fill(243, 179, 194);
+  circle(230,340,60);
+  circle(470,340,60);
+  
+  stroke(0,0,0);
+  line(270,400,350,450);
+  line(350,450,430,400);
+}
